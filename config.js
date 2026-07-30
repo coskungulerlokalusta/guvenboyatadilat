37545310a035b524683b2633e6fef34b205b5a21
coskungulerlokalusta <coskungulerrrr@gmail.com>
2026-07-30 15:32:49 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -7,4 +7,4 @@
  * gerek yoktur; tüm site (form, panel, telefon/whatsapp linkleri,
  * blog, fotoğraflar) buradan okur.
  */
-window.GUVENBOYA_API_URL = "https://script.google.com/macros/s/AKfycbx7ybEzp7DMATmsj4OWUNFXOe1eEc3LbalTRvbcUzuREnnEBcufCZjW2NtP8eEz8xpXtQ/exec";
+window.GUVENBOYA_API_URL = null; // örn: "https://script.google.com/macros/s/AKfycb.../exec"
